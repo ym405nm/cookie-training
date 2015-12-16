67abd833aa75a6ddefd79bec4713a52bbc4c8c2e
ym405nm <nsd08888@gmail.com>
2015-12-16 14:14:57 +0900

    cookie設定

--- sample1.php
+++ sample1.php
@@ -1,1 +1,1 @@
-<?php setcookie("training", "training-value"); ?>Cookieをセットしました
+<?php setcookie("kobe-training", "this is a kobe-training cookie", time()+3600, "sample1.php", $_SERVER["HTTP_HOST"], 1, 1); ?>Cookieをセットしました
